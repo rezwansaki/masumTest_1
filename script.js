6bf7b2009fec6e752df9db1ef47dae0c904af9c2
Md. Rezwan Saki Alin <rezwansaki@gmail.com>
2016-07-26 15:08:21 +0600

    seperate scripts from html file

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+ $("#btn1").click(function(){
+    $("form").append("<textarea rows='10' cols='40'>");
+    $("form").append("<input type='button' value='Send'>");
+ });
+});
