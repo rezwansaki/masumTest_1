bb1ae70f06989fcc20fd4ce2577f6fc338ff2f19
Md. Rezwan Saki Alin <rezwansaki@gmail.com>
2016-07-26 16:21:50 +0600

    add form validation and extra fields

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 $(document).ready(function(){
- $("#btn1").click(function(){
-    $("form").append("<textarea rows='10' cols='40'>");
-    $("form").append("<input type='button' value='Send'>");
- });
-});
+    
+  $("#btnSend").hide();
+     
+     //open mail system 
+     $("#btn1").click(function(){
+        $("form").append("Send To : <input type='input' id='txtSentTo'>");
+        $("form").append("Subject : <input type='input' id='txtSubject'>");
+        $("form").append("Mail Body : <textarea rows='10' cols='40' id='txtMailBody'>");
+        $("#btn1").hide();
+        $("#btnSend").show();
+     });
+    
+    //send button
+    $("#btnSend").click(function(){
+        
+        //form validation
+        var xSendTo = $(txtSentTo).val();
+        var xSubject = $(txtSubject).val();
+        var xMailBody = $(txtMailBody).val();
+        if(xSendTo == "" || xSubject == "" || xMailBody == ""){
+            alert("Please fill the required fields!!");
+        }else
+        {
+            alert("Successfully send !!");
+            $("form").empty();
+            $("#txtSentTo").remove();
+            $("#txtSubject").remove();
+            $("#txtMailBody").remove();
+            $("#btn1").show();
+            $("#btnSend").hide();
+        }
+        
+    });
+    
+}); //end of ready function 
